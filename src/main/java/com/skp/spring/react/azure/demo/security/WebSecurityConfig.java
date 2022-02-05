00bf9441e6131aa94cc88a80a14f216111b10d0f
Sumesh KP <sumesh.kp@trenser.com>
2022-02-05 21:06:57 +0530

    Added authorization2

--- src/main/java/com/skp/spring/react/azure/demo/security/WebSecurityConfig.java
+++ src/main/java/com/skp/spring/react/azure/demo/security/WebSecurityConfig.java
@@ -75,7 +75,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-                .antMatchers("/api/**").permitAll()
             .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
