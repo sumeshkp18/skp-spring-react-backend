f8bbee5f6892cc45f00ba018a4d1672f266ec21d
Sumesh KP <sumesh.kp@trenser.com>
2022-02-08 10:17:24 +0530

    add hom api url

--- src/main/java/com/skp/spring/react/azure/demo/security/WebSecurityConfig.java
+++ src/main/java/com/skp/spring/react/azure/demo/security/WebSecurityConfig.java
@@ -75,6 +75,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/hello/**").permitAll()
             .anyRequest().authenticated();
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
